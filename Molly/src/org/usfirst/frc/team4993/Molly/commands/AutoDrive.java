f02326cf79a9d39723fb7a4e88ff05249ac80e40
KenzieShay <shayblue17@outlook.com>
2015-04-03 14:34:01 -0300

    Moved code to Initialize method from Execute method

--- Molly/src/org/usfirst/frc/team4993/Molly/commands/AutoDrive.java
+++ Molly/src/org/usfirst/frc/team4993/Molly/commands/AutoDrive.java
@@ -23,11 +23,12 @@ public class  AutoDrive extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	DriveTrain.AuDrive();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	DriveTrain.AuDrive();
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
